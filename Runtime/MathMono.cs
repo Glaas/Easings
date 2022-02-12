c501462e12863577021b7fd83e8c7f4839f95da7
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2022-02-12 16:10:00 +0100

    Monobehavior

---
+++ Runtime/MathMono.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathMono : MonoBehaviour
+{
+    public void MyFunction()
+    {
+        Debug.Log("Hello World");
+    }
+}
