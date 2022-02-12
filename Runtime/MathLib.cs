504698023ffb4943933b2feff231ec4cf72eb8e2
Sebastien "Glaas" Decuyper <s.decuyper314@gmail.com>
2022-02-12 17:19:29 +0100

    updated package.json
    package json again
    
    Monobehavior
    
    added namespace

--- Runtime/MathLib.cs
+++ Runtime/MathLib.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace SebEssentials
 {
     public static class MathLib<T>
---
+++ Runtime/MathMono.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SebEssentials
+{
+
+
+    public class MathMono : MonoBehaviour
+    {
+        public void MyFunction()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
